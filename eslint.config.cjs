82abab4420cc45ff529b5763dc2a7d2ab1322744
nicholasevansitanggang <5026231146@student.its.ac.id>
2026-05-31 14:45:28 +0700

    ci: fix ESLint config and CI lint step

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,6 +1,16 @@
 // Minimal flat config that re-uses eslint-config-next
 try {
-  module.exports = [require('eslint-config-next')];
+  const nextConfig = require('eslint-config-next');
+  // `eslint-config-next` may export either an object or an array of configs.
+  // ESLint flat config expects either an object or an array of objects, but
+  // nesting arrays can produce "Unexpected array" errors. Normalize here.
+  if (Array.isArray(nextConfig)) {
+    module.exports = nextConfig;
+  } else if (nextConfig && typeof nextConfig === 'object') {
+    module.exports = [nextConfig];
+  } else {
+    module.exports = [];
+  }
 } catch (e) {
   // If eslint-config-next can't be required for some reason, export a fallback empty config
   module.exports = [];
